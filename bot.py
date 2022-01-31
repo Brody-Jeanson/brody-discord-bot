aa0563d6286e3915e5332367d2c321786ce44e7d
Brody <brody.jeanson@gmail.com>
2022-01-30 19:55:34 -0800

    Add bot presence

--- bot.py
+++ bot.py
@@ -11,6 +11,6 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
-
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name='Chunes'))
+    user = client.get_user('381870129706958858')
 client.run(TOKEN)
-
